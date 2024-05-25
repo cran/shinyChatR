284d6dae1be26285d687bea30c8e30fab8988b4d
Julian Schmocker <julian.schmocker@gmail.com>
2024-05-25 10:30:02 +0000

    version 1.2.0

--- inst/doc/shinyChatR.R
+++ inst/doc/shinyChatR.R
@@ -1,4 +1,4 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>",
@@ -30,7 +30,7 @@ knitr::opts_chunk$set(
 #  server <- function(input, output, server) {
 #    chat_server("test", db_connection = conn,
 #                db_table_name = "chat_data",
-#                chat_user = "user1")
+#                chat_user = "user2")
 #  }
 #  
 #  # Run the application
@@ -84,6 +84,48 @@ knitr::opts_chunk$set(
 #                rds_path = test_rds,
 #                chat_user = "user2")
 #  }
+#  
+#  # Run the application
+#  shinyApp(ui = ui, server = server)
+
+## -----------------------------------------------------------------------------
+#  # store db as tempfile. Select different path to store db locally
+#  tempdb <- file.path(tempdir(), "db")
+#  
+#  con <- dbConnect(RSQLite::SQLite(), tempdb)
+#  
+#  ui <- fluidPage(
+#  
+#    fluidRow(
+#      column(width = 6,
+#             # add chat ui elements
+#             chat_ui("test1"),
+#      )
+#    ),
+#    br(),
+#    fluidRow(
+#      column(width = 6,
+#             # add button to update text value
+#             actionButton("set_value_abc", "Set text to 'abc'"),
+#      )
+#    ),
+#  )
+#  
+#  
+#  server <- function(input, output, server) {
+#  
+#    # corresponding server part for id test1
+#    chat_server("test1", db_connection = con,
+#                db_table_name = "chat_data",
+#                chat_user = "user1"
+#                )
+#  
+#    observeEvent(input$set_value_abc,{
+#      updateChatTextInput(id = "test1",
+#                          value = "abc")
+#    })
+#  }
+#  
 #  
 #  # Run the application
 #  shinyApp(ui = ui, server = server)
